0d4f6574bfea4f9ad6f7d2609a43f0cc371f5476
youssef62 <youssef.bou62@gmail.com>
2024-12-03 16:22:13 +0100

    added dataset.py containing Dataset object and loader details

--- src/data/prepare_data.py
+++ src/data/prepare_data.py
@@ -7,7 +7,7 @@ import numpy as np
 import argparse 
 from tqdm import tqdm
 import torch 
-from torch.utils.data import Dataset 
+from src.data.dataset import CustomChessScoreSheetsDataset
 
 chess_reader_data_path = "data/chess_reader_dataset" 
 online_data_path = "data/online_dataset" 
@@ -66,17 +66,6 @@ def load_images_labels(path):
     return images, labels 
 
 
-class CustomChessScoreSheetsDataset(Dataset):
-    def __init__(self, images, labels): 
-        self.images = [torch.tensor(img, dtype=torch.float32) for img in images]
-        self.labels = labels
-    def __len__(self):
-        return len(self.images)
-    
-    def __getitem__(self, idx):
-        image = self.images[idx]
-        label = self.labels[idx]
-        return image, label
     
 def shuffle(a,b):
     assert len(a) == len(b) 
