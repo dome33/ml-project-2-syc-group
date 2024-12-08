f181efc87bade7ddede985e944e2662349d3a4cb
youssef62 <youssef.bou62@gmail.com>
2024-12-08 09:50:58 +0100

    add custon ds to trainset

--- src/data/prepare_data.py
+++ src/data/prepare_data.py
@@ -12,7 +12,7 @@ ONLINE_SPLIT = 0.9
 # Paths to datasets
 chess_reader_data_path = "data/chess_reader_dataset"
 online_data_path = "data/online_dataset"
-
+custom_data_path = "data/custom_dataset"
 
 # Function to load images and labels
 def load_images_labels(path):
@@ -30,22 +30,26 @@ def load_images_labels(path):
 if __name__ == "__main__":
     chess_reader_dataset = load_images_labels(chess_reader_data_path) 
     online_dataset = load_images_labels(online_data_path)
+    custom_dataset = load_images_labels(custom_data_path)
+    
     
     random.shuffle(chess_reader_dataset) 
     random.shuffle(online_dataset)
+    random.shuffle(custom_dataset)
     
     # TEST 
     split = int(len(chess_reader_dataset) * CHESS_READER_SPLIT) 
     np.save("data/testset.npy", chess_reader_dataset[:split]) 
     
     # VALIDATION 
-    valdataset = chess_reader_dataset[split:]
+    valdataset = chess_reader_dataset[split:] 
     split = int(len(online_dataset) * ONLINE_SPLIT)
     valdataset += online_dataset[split:] 
+    
     np.save("data/valset.npy", valdataset) 
     
     # TRAIN 
-    train_dataset = online_dataset[:split]
+    train_dataset = online_dataset[:split] + custom_dataset 
     random.shuffle(train_dataset) 
     np.save("data/trainset.npy", train_dataset) 
     
