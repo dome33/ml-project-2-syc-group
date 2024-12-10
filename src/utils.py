aba479aa18fef1f2061a75adc98aca317350fde7
youssef62 <youssef.bou62@gmail.com>
2024-12-10 18:43:14 +0100

    added htr_net

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,28 @@
-from mltu.torch.dataProvider import DataProvider
+import torch
+import torch.nn as nn
+from mltu.torch.losses import CTCLoss
+from mltu.torch.metrics import CERMetric , WERMetric
+# CTLoss from mltu adapted to work with the 
+# HTR best practices model  
+class CTCLossShortcut(CTCLoss): 
+    # adapted to work with the HTR best practices model 
+    # that has a shortcut connection 
+    def forward(self,output,target): 
+        if len(output) == 2: 
+            return super().forward(output[0],target) + 0.1 * super().forward(output[1],target)
+        else : 
+            return super().forward(output,target)
+        
+class CERMetricShortCut(CERMetric): 
+    
+    def update(self, output: torch.Tensor, target: torch.Tensor, **kwargs) -> None:
+        if len(output) == 2: 
+            output = output[0]
+        super().update(output, target, **kwargs)
 
 
-def split_data_provider(dataprovider,split=0.9): 
-    split = int(len(dataprovider._dataset) * split) 
-    train_dataProvider = DataProvider(
-        dataset=dataprovider._dataset[:split],
-        batch_size=dataprovider._batch_size,
-        data_preprocessors=dataprovider._data_preprocessors,
-        transformers=dataprovider._transformers,
-        use_cache=dataprovider._use_cache,
-    )
-
-    test_dataProvider = DataProvider(
-        dataset=dataprovider._dataset[split:],
-        batch_size=dataprovider._batch_size,
-        data_preprocessors=dataprovider._data_preprocessors,
-        transformers=dataprovider._transformers,
-        use_cache=dataprovider._use_cache,
-    )
-    return train_dataProvider, test_dataProvider 
+class WERMetricShortCut(WERMetric):
+    def update(self, output: torch.Tensor, target: torch.Tensor, **kwargs) -> None:
+        if len(output) == 2: 
+            output = output[0]
+        super().update(output, target, **kwargs)
